9990d55de061917de0af68c540e2ad75f19faf06
Tsuyoshi Yoshida <holmes21th@gmail.com>
2019-05-01 19:45:34 +0900

    add: Markdown Table Generator ベース作成

---
+++ src/components/Mtg.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="app">
+    <!-- タイトル -->
+    <div class="title">
+      <h1>Markdown Table Generator</h1>
+    </div>
+    <!-- 操作ボタン -->
+    <div class="controlButtons">
+      <button v-on:click="addRow">Add Row</button>
+      <button v-on:click="addColumn">Add Column</button>
+      <button v-on:click="reset">Reset</button>
+    </div>
+    <!-- テーブルのヘッド部 -->
+    <table v-if="tableHeadData.length > 0" class="tableHeader">
+      <thead>
+        <tr>
+          <th></th>
+          <th v-for="(col, index) in tableHeadData" v-bind:key="index">
+            <button>Del Col</button>
+          </th>
+          <th></th>
+        </tr>
+        <tr>
+          <th></th>
+          <th v-for="(col, index) in tableHeadData" v-bind:key="index">
+            <button>←</button>
+            <button>→</button>
+          </th>
+          <th></th>
+        </tr>
+        <tr>
+          <th>
+            <button>↑</button>
+            <button>↓</button>
+          </th>
+          <th v-for="(col, index) in tableHeadData" v-bind:key="index">
+            <input v-model="tableHeadData[index]" placeholder="Input text" />
+          </th>
+          <th>
+            <button>Del Row</button>
+          </th>
+        </tr>
+      </thead>
+    </table>
+    <!-- テーブルのボディ部 -->
+    <table v-if="tableBodyDatas.length > 0" class="tableBody">
+      <tbody>
+        <tr v-for="(row, index) in tableBodyDatas" v-bind:key="index">
+          <th>
+            <button>↑</button>
+            <button>↓</button>
+          </th>
+          <th v-bind:value="row" v-for="(col, colIndex) in row" v-bind:key="colIndex">
+            <input v-model="row[colIndex]" placeholder="Input text" />
+          </th>
+          <th>
+            <button>Del Row</button>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+    <!-- Markdown 形式のデータ出力 -->
+    <div class="mdTextbox">
+      <textarea v-model="formattedTableDatas" cols="100" rows="10">
+      </textarea>
+    </div>
+    <!-- 出力ボタン -->
+    <div class="exportButtons">
+      <button>Copy to Clipboard</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      tableDatas: [
+      ["", ""],
+      ["", ""]
+    ]
+    }
+  },
+  methods: {
+    addRow: function() {
+      this.tableDatas = [...this.tableDatas, [...this.tableDatas[0].map(_ => "")]]
+    },
+    addColumn: function() {
+      this.tableDatas = this.tableDatas.map(row => [...row, ""])
+    },
+    reset: function() {
+      this.tableDatas = [
+        [ "", "" ],
+        [ "", "" ]
+      ]
+    }
+  },
+  computed: {
+    // ヘッド部に表示するデータ
+    tableHeadData() {
+      return this.tableDatas[0]
+    },
+    // ボディ部に表示するデータ
+    tableBodyDatas() {
+      return this.tableDatas.slice(1)
+    },
+    // テキストボックスに表示するテキストデータ生成
+    formattedTableDatas() {
+      return this.tableDatas.map((row, index) => {
+        const colText = `| ${row.map(col => col).join(" | ")} |`
+        // 2行目以降はデータ部のみ
+        if (index > 0) return colText
+        // 1行目はヘッドなのでセパレータも返す
+        const separator = `| ${row.map(_ => "---").join(" | ")} |`
+        return `${colText}\n${separator}`
+      }).join("\n")
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
