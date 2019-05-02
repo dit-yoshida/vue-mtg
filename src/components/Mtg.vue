cf27068dc3b099529d896df1806b0983676f4dd7
Tsuyoshi Yoshida <holmes21th@gmail.com>
2019-05-02 17:35:18 +0900

    update: 行列移動機能実装

--- src/components/Mtg.vue
+++ src/components/Mtg.vue
@@ -23,15 +23,29 @@
         <tr>
           <th></th>
           <th v-for="(col, index) in tableHeadData" v-bind:key="index">
-            <button>←</button>
-            <button>→</button>
+            <button
+              :disabled="index === 0"
+              v-on:click="moveColumnToLeft(index)"
+            >
+              ←
+            </button>
+            <button
+              :disabled="index === tableHeadData.length - 1"
+              v-on:click="moveColumnToRight(index)"
+            >
+              →
+            </button>
           </th>
           <th></th>
         </tr>
         <tr>
           <th>
-            <button>↑</button>
-            <button>↓</button>
+            <button disabled>
+              ↑
+            </button>
+            <button v-on:click="moveRowToDown(0)">
+              ↓
+            </button>
           </th>
           <th v-for="(col, index) in tableHeadData" v-bind:key="index">
             <input v-model="tableHeadData[index]" placeholder="Input text" />
@@ -47,8 +61,15 @@
       <tbody>
         <tr v-for="(row, index) in tableBodyDatas" v-bind:key="index">
           <th>
-            <button>↑</button>
-            <button>↓</button>
+            <button v-on:click="moveRowToUp(index + 1)">
+              ↑
+            </button>
+            <button
+              :disabled="index===tableBodyDatas.length - 1"
+              v-on:click="moveRowToDown(index + 1)"
+            >
+              ↓
+            </button>
           </th>
           <th v-bind:value="row" v-for="(col, colIndex) in row" v-bind:key="colIndex">
             <input v-model="row[colIndex]" placeholder="Input text" />
@@ -108,6 +129,38 @@ export default {
       const newTableDatas = this.tableDatas.map((row) => row.filter((_, index) => index !== columnIndex))
       // 列がなくなった場合は空配列にする
       this.tableDatas = newTableDatas[0].length > 0 ? newTableDatas : []
+    },
+    moveColumnToLeft: function(columnIndex) {
+      if (columnIndex == 0) return
+      this.tableDatas = this.tableDatas.map(row => {
+        let newRow = [...row]
+        const targetColumn = newRow.splice(columnIndex, 1)[0]
+        newRow.splice(columnIndex - 1, 0, targetColumn)
+        return newRow
+      })
+    },
+    moveColumnToRight: function(columnIndex) {
+      if (columnIndex == this.tableDatas[0].length - 1) return
+      this.tableDatas = this.tableDatas.map(row => {
+        let newRow = [...row]
+        const targetColumn = newRow.splice(columnIndex, 1)[0]
+        newRow.splice(columnIndex + 1, 0, targetColumn)
+        return newRow
+      })
+    },
+    moveRowToUp: function(rowIndex) {
+      if (rowIndex == 0) return
+      let newTableDatas = [...this.tableDatas]
+      const targetRow = newTableDatas.splice(rowIndex, 1)[0]
+      newTableDatas.splice(rowIndex - 1, 0, targetRow)
+      this.tableDatas = newTableDatas
+    },
+    moveRowToDown: function(rowIndex) {
+      if (rowIndex == this.tableDatas.length - 1) return
+      let newTableDatas = [...this.tableDatas]
+      const targetRow = newTableDatas.splice(rowIndex, 1)[0]
+      newTableDatas.splice(rowIndex + 1, 0, targetRow)
+      this.tableDatas = newTableDatas
     },
     copyToClipboard: function() {
       this.$copyText(this.formattedTableDatas).then(() => {}, () => {})
